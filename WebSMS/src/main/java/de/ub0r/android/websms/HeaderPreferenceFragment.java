a5b35916156eff346e1cc5c3605412ee8c7e8e73
Mark Smith <mark@chez-moi.org.uk>
2014-01-01 15:26:08 +0000

    4.7.2 updates to allow write to SMS database on Kitkat with appropriate App Ops permission set. Also validate Preferences fragment as required where target API>=19.

--- WebSMS/src/main/java/de/ub0r/android/websms/HeaderPreferenceFragment.java
+++ WebSMS/src/main/java/de/ub0r/android/websms/HeaderPreferenceFragment.java
@@ -20,7 +20,9 @@ package de.ub0r.android.websms;
 
 import android.app.Activity;
 import android.content.Context;
+import android.os.Build;
 import android.os.Bundle;
+import android.preference.Preference;
 import android.preference.PreferenceFragment;
 import de.ub0r.android.lib.IPreferenceContainer;
 
@@ -47,6 +49,14 @@ public final class HeaderPreferenceFragment extends PreferenceFragment
 		PreferencesActivity.registerPreferenceChecker(this);
 		PreferencesActivity.addConnectorPreferences(this);
 		PreferencesActivity.registerOnPreferenceChangeListener(this);
+
+        // MDS - Override summary for drop_sent preference on Kitkat or later to
+        // indicate that App Ops is needed to set WRITE_SMS permission
+        if (Build.VERSION.SDK_INT > Build.VERSION_CODES.JELLY_BEAN_MR2) {
+            Preference pref = this.findPreference("drop_sent");
+            pref.setSummary(this.getString(R.string.drop_sent_hint_kitkat));
+        }
+
 	}
 
 	/**
